6a8fe9da6339896173a8a2cfd04543f74a2704b1
Gauri <mail.gauri08@gmail.com>
2020-09-09 18:07:19 +0530

    Initial commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import UserItems from './UserItems';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types'
+
+const Users = ({users , loading}) => {
+    if(loading)
+    {
+        return(<Spinner/>)
+    } 
+    else{
+  
+        return (
+            <div style = {UserStyle}>
+                {users.map(user => (
+                    <UserItems key = {user.id} user = {user} />
+                ))}
+            </div>
+        );
+    } 
+    
+}
+
+Users.propTypes = {
+    users : PropTypes.array.isRequired,
+    loading : PropTypes.bool.isRequired,
+}
+
+const UserStyle  = {
+    display : 'grid',
+    gridTemplateColumns : 'repeat(3,1fr)',
+    gridGap : '1rem'
+}
+
+export default Users;
